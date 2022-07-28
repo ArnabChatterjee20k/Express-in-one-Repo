83beaf55cf4761ee1b9f56635d75a15c184a4ad2
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-07-28 11:01:51 +0530

    added advanced routing

--- routes/users.js
+++ routes/users.js
@@ -16,4 +16,12 @@ router.get("/:id",(req,res)=>{
 
 // Express match the routes one by one /new and /:id will be treated same. So put static routes above dynamic routes
 // router.get("/new")
+
+
+// We can use router.get("/") and router.post("/") to do for a same route
+
+// Chaining to solve this
+router.route("/chain")
+.get((req,res)=>res.send("ee"))
+.post((req,res)=>res.send("hie"))
 module.exports = router
